6d254e9f5b9177da4c9084974743ab0d7b4eb438
Roger Zhang <ruojiazh@amazon.com>
2023-05-23 10:49:07 -0700

    refactor to standalone metric class

--- aws_lambda_powertools/metrics/provider/__init__.py
+++ aws_lambda_powertools/metrics/provider/__init__.py
@@ -1,9 +1,21 @@
-from aws_lambda_powertools.metrics.provider.base import MetricsProvider
+from aws_lambda_powertools.metrics.provider.base import (
+    MetricsBase,
+    MetricsProvider,
+    MetricsProviderBase,
+)
 from aws_lambda_powertools.metrics.provider.cloudwatchemf_provider import (
     CloudWatchEMFProvider,
+)
+from aws_lambda_powertools.metrics.provider.datadog_provider_test import (
+    DataDogMetrics,
+    DataDogProvider,
 )
 
 __all__ = [
     "CloudWatchEMFProvider",
     "MetricsProvider",
+    "MetricsBase",
+    "MetricsProviderBase",
+    "DataDogMetrics",
+    "DataDogProvider",
 ]
